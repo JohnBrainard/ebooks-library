39560732e184b430f4d8a2364c631141732ffc54
john <jfbrainard@gmail.com>
2021-12-23 13:47:28 -0700

    configure datasource and connection pool

--- ebooks-library-api/build.gradle.kts
+++ ebooks-library-api/build.gradle.kts
@@ -1,7 +1,9 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val hikari_version: String by project
 val koin_version: String by project
+val kotlin_version: String by project
+val ktor_version: String by project
+val logback_version: String by project
+val postgres_version: String by project
 
 plugins {
 	application
@@ -26,8 +28,12 @@ dependencies {
 	implementation("io.ktor:ktor-utils:$ktor_version")
 	implementation("io.ktor:ktor-jackson:$ktor_version")
 	implementation("io.insert-koin:koin-ktor:$koin_version")
-	implementation(project(":ebooks-library-core"))
 	implementation("ch.qos.logback:logback-classic:$logback_version")
+
+	implementation(project(":ebooks-library-core"))
+
+	implementation("com.zaxxer:HikariCP:$hikari_version")
+	runtimeOnly("org.postgresql:postgresql:$postgres_version")
 }
 
 application {
--- ebooks-library-api/src/main/kotlin/dev/johnbrainard/ebooks/api/ApplicationModule.kt
+++ ebooks-library-api/src/main/kotlin/dev/johnbrainard/ebooks/api/ApplicationModule.kt
@@ -1,5 +1,7 @@
 package dev.johnbrainard.ebooks.api
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import dev.johnbrainard.ebooks.EbookCollectionRepository
 import dev.johnbrainard.ebooks.EbookMetaRepository
 import dev.johnbrainard.ebooks.api.plugins.CollectionsService
@@ -9,13 +11,35 @@ import dev.johnbrainard.ebooks.files.FilesEbookMetaRepository
 import dev.johnbrainard.ebooks.meta.PdfMetaExtractor
 import dev.johnbrainard.ebooks.meta.pdfbox.PdfBoxMetaExtractor
 import org.koin.dsl.module
+import javax.sql.DataSource
 
 val applicationModule = module {
 	single<EbookCollectionRepository> {
 		FilesEbookCollectionRepository(getProperty("LIBRARY_PATH"))
 	}
 
+//	single<EbookCollectionRepository> {
+//		DbCollectionRepository(get())
+//	}
+
 	single<EbookMetaRepository> { FilesEbookMetaRepository(get()) }
 	single<PdfMetaExtractor> { PdfBoxMetaExtractor() }
 	single<CollectionsService> { DefaultCollectionsService(get(), get()) }
+
+	single<DataSource> {
+		HikariDataSource(get())
+	}
+
+	single {
+		HikariConfig().apply {
+			driverClassName = "org.postgresql.Driver"
+			jdbcUrl = getProperty("DB_URL")
+			username = getProperty("DB_USER")
+			password = getProperty("DB_PASSWORD")
+			maximumPoolSize = 3
+			isAutoCommit = false
+			transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+			validate()
+		}
+	}
 }
